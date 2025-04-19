05b558c49d98ddc1ef80a504a600d8b118c08b1a
DarthBane <pleiva1138@gmail.com>
2025-04-19 13:19:36 -0400

    Se agrega endpoint obtención alimentos

--- routes/mantenedor_alimento.js
+++ routes/mantenedor_alimento.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 app.use(express.json())
 app.use(bodyParser.json());
 
+// Endpoint obtención 
 router.get('/grupos_alimenticios', async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -20,6 +21,21 @@ router.get('/grupos_alimenticios', async (req, res) => {
         res.status(500).json({ error: 'Error al consultar grupos alimenticios' });
       }
 });
+
+// Endpoint obtención 
+router.get('/alimentos', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    try {
+        const result = await pool.query(`
+          SELECT * FROM obtener_alimento()`);
+          res.json(result.rows);
+      } catch (err) {
+        console.error('Error al consultar alimentos:', err);
+        res.status(500).json({ error: 'Error al consultar alimentos' });
+      }
+});
+
 
 app.use(router);
 module.exports = router
